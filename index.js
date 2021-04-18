88c5a2bbc147ce2e963b52bb6627c64f545c84e7
KuryKat <57152799+KuryKat@users.noreply.github.com>
2021-04-18 03:25:51 -0300

    Criando principal Handler de Erros ❌
    - Handler para lidar com os erros arremessados pelo NextFunction
    - Atualizando caminho de arquivos

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import cors from 'cors'
 import { join } from 'path'
 import { readFileSync } from 'fs'
 import { BotModel, UserModel } from './modules/database/database.module.js'
-import userModule from './modules/user.module.js'
-import botModule from './modules/bot.module.js'
+import userModule from './modules/users/user.module.js'
+import botModule from './modules/bots/bot.module.js'
 
 const config = JSON.parse(readFileSync(join(process.cwd(), 'config.json')))
 
@@ -15,5 +15,13 @@ app.use(express.json())
 
 app.use('/bots', botModule(BotModel))
 app.use('/users', userModule(UserModel))
+
+app.use((err, req, res, next) => {
+  res.status(err.status)
+  res.send({
+    code: err.status,
+    message: err.message
+  })
+})
 
 app.listen(config.port, console.log('listen on port ' + config.port))
